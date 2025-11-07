2ee83ab960a6b87aaa748e3ac91eb4d06238ad87
MHDante <hello@mhdante.com>
2025-11-07 16:01:53 -0500

    Unparent upgrades

--- Unity/Runtime/Types/UnparentOnEnable.cs
+++ Unity/Runtime/Types/UnparentOnEnable.cs
@@ -7,7 +7,7 @@ namespace UnityJigs
 {
     public class UnparentOnEnable : MonoBehaviour
     {
-        [ReadOnly]public Transform Parent = null!;
+        [ReadOnly] public Transform Parent = null!;
         public bool RevertOnDisable = false;
 
         [Header("Rigidbodies")]
@@ -15,49 +15,66 @@ namespace UnityJigs
         [ShowIf(nameof(MyRigidbody))]
         public Rigidbody? ParentRigidbody;
 
+
+        [ShowIf(nameof(MyRigidbody))]
+        public bool ClearExclusions;
+        [ShowIf(nameof(MyRigidbody))] [ShowIf(nameof(ClearExclusions))]
+        public LayerMask NewExclusionLayer;
+        [ShowIf(nameof(MyRigidbody))] [ShowIf(nameof(ClearExclusions))]
+        public float ClearExclusionsDelay = 0f;
+
         [Header("Layer Swap")]
         public bool SwapLayer = false;
         [ShowIf(nameof(SwapLayer))]
-        [Layer]public int NewLayer;
+        [Layer] public int NewLayer;
         [ShowIf(nameof(SwapLayer)), MinValue(0)]
         public float LayerSwapDelay = 0f;
 
         private float _unparentTime = 0f;
         private int _oldLayer;
+        private LayerMask _oldExclusionLayer;
 
         private void Reset()
         {
-            if(!MyRigidbody)MyRigidbody = GetComponent<Rigidbody>();
-            if(!ParentRigidbody)MyRigidbody = GetComponentInParent<Rigidbody>();
+            if (!MyRigidbody) MyRigidbody = GetComponent<Rigidbody>();
+            if (!ParentRigidbody) MyRigidbody = GetComponentInParent<Rigidbody>();
         }
 
-        private void OnValidate() => Parent =  transform.parent;
+        private void OnValidate() => Parent = transform.parent;
+
         private void OnEnable()
         {
-            _oldLayer = gameObject.layer;
             transform.parent = null;
             _oldLayer = gameObject.layer;
             if (SwapLayer && LayerSwapDelay <= 0) gameObject.layer = NewLayer;
             _unparentTime = Time.time;
-            if (MyRigidbody && ParentRigidbody)
+            if (MyRigidbody)
             {
-                MyRigidbody.linearVelocity = ParentRigidbody.linearVelocity;
-                MyRigidbody.angularVelocity = ParentRigidbody.angularVelocity;
+                _oldExclusionLayer = MyRigidbody.excludeLayers;
+                if (ClearExclusions && ClearExclusionsDelay <= 0) MyRigidbody.excludeLayers = NewExclusionLayer;
+                if (ParentRigidbody)
+                {
+                    MyRigidbody.linearVelocity = ParentRigidbody.linearVelocity;
+                    MyRigidbody.angularVelocity = ParentRigidbody.angularVelocity;
+                }
             }
         }
 
         private void Update()
         {
             if (SwapLayer && Time.time > _unparentTime + LayerSwapDelay) gameObject.layer = NewLayer;
+            if (MyRigidbody && ClearExclusions && Time.time > _unparentTime + ClearExclusionsDelay)
+                MyRigidbody.excludeLayers = NewExclusionLayer;
         }
 
         private void OnDisable()
         {
-            if(!RevertOnDisable) return;
+            if (!RevertOnDisable) return;
             transform.parent = Parent ? Parent : null;
             gameObject.layer = _oldLayer;
             if (MyRigidbody)
             {
+                MyRigidbody.excludeLayers = _oldExclusionLayer;
                 MyRigidbody.linearVelocity = Vector3.zero;
                 MyRigidbody.angularVelocity = Vector3.zero;
             }
